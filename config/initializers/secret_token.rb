655d8d8ab460ea6489c34e0f74f2a57611ad984f
lucidstory <lucidstory_ruby@outlook.com>
2016-03-28 12:48:24 +0800

    NEW_Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails305::Application.config.secret_token = 'c3cccc9b5a86ebdcacc4e0ca07237b50703947dc5600c7bf977499e7f05200a5fe13a4930b230bafaf4915822274cdcffa3765f7cedcd8e7335e79eb5df53abe'
