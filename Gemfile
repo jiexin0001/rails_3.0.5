8cdf32e58f28a86e4bef5b742242de4c7992f852
lucidstory <lucidstory_ruby@outlook.com>
2016-03-28 14:00:24 +0800

    Simple Cart Created

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
-source 'http://rubygems.org'
+source 'https://ruby.taobao.org'
 
 gem 'rails', '3.0.5'
-
+gem 'rake', '~>0.8.7'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails305::Application.routes.draw do
+  resources :line_items
+
+  resources :carts
+
+  get "store/index"
+
+  resources :products
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,7 +57,7 @@ Rails305::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
-
+    root :to => 'store#index', :as => 'store'
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
